dacc390929eae01f37b7be1d922402f385641c46
Anil <murat.anil.oener@gmail.com>
2022-10-21 13:00:26 +0200

    refactor and separate completely

--- modular/b9-checkers-academy-draft/x/checkers/keeper/player_info_handler.go
+++ modular/b9-checkers-academy-draft/x/checkers/keeper/player_info_handler.go
@@ -6,46 +6,8 @@ import (
 	rules "github.com/b9lab/checkers/x/checkers/rules"
 	"github.com/b9lab/checkers/x/checkers/types"
 
-	leaderboard "github.com/b9lab/checkers/x/leaderboard/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
-
-func mustAddDeltaGameResultToPlayer(
-	k *Keeper,
-	ctx sdk.Context,
-	player sdk.AccAddress,
-	wonDelta uint64,
-	lostDelta uint64,
-	forfeitedDelta uint64,
-) (playerInfo leaderboard.PlayerInfo) {
-	playerInfo, found := k.board.GetPlayerInfo(ctx, player.String())
-	if !found {
-		playerInfo = leaderboard.PlayerInfo{
-			Index:          player.String(),
-			WonCount:       0,
-			LostCount:      0,
-			ForfeitedCount: 0,
-			DateUpdated:    ctx.BlockTime().UTC().Format(types.DeadlineLayout),
-		}
-	}
-	playerInfo.WonCount += wonDelta
-	playerInfo.LostCount += lostDelta
-	playerInfo.ForfeitedCount += forfeitedDelta
-	k.board.SetPlayerInfo(ctx, playerInfo)
-	return playerInfo
-}
-
-func (k *Keeper) MustAddWonGameResultToPlayer(ctx sdk.Context, player sdk.AccAddress) leaderboard.PlayerInfo {
-	return mustAddDeltaGameResultToPlayer(k, ctx, player, 1, 0, 0)
-}
-
-func (k *Keeper) MustAddLostGameResultToPlayer(ctx sdk.Context, player sdk.AccAddress) leaderboard.PlayerInfo {
-	return mustAddDeltaGameResultToPlayer(k, ctx, player, 0, 1, 0)
-}
-
-func (k *Keeper) MustAddForfeitedGameResultToPlayer(ctx sdk.Context, player sdk.AccAddress) leaderboard.PlayerInfo {
-	return mustAddDeltaGameResultToPlayer(k, ctx, player, 0, 0, 1)
-}
 
 func getWinnerAndLoserAddresses(storedGame *types.StoredGame) (winnerAddress sdk.AccAddress, loserAddress sdk.AccAddress) {
 	if storedGame.Winner == rules.PieceStrings[rules.NO_PLAYER] {
@@ -71,14 +33,14 @@ func getWinnerAndLoserAddresses(storedGame *types.StoredGame) (winnerAddress sdk
 	return winnerAddress, loserAddress
 }
 
-func (k *Keeper) MustRegisterPlayerWin(ctx sdk.Context, storedGame *types.StoredGame) (winnerInfo leaderboard.PlayerInfo, loserInfo leaderboard.PlayerInfo) {
+func (k *Keeper) MustRegisterPlayerWin(ctx sdk.Context, storedGame *types.StoredGame) {
 	winnerAddress, loserAddress := getWinnerAndLoserAddresses(storedGame)
-	return k.MustAddWonGameResultToPlayer(ctx, winnerAddress),
-		k.MustAddLostGameResultToPlayer(ctx, loserAddress)
+	k.board.MustAddWonGameResultToPlayer(ctx, winnerAddress)
+	k.board.MustAddLostGameResultToPlayer(ctx, loserAddress)
 }
 
-func (k *Keeper) MustRegisterPlayerForfeit(ctx sdk.Context, storedGame *types.StoredGame) (winnerInfo leaderboard.PlayerInfo, forfeiterInfo leaderboard.PlayerInfo) {
+func (k *Keeper) MustRegisterPlayerForfeit(ctx sdk.Context, storedGame *types.StoredGame) {
 	winnerAddress, loserAddress := getWinnerAndLoserAddresses(storedGame)
-	return k.MustAddWonGameResultToPlayer(ctx, winnerAddress),
-		k.MustAddForfeitedGameResultToPlayer(ctx, loserAddress)
+	k.board.MustAddWonGameResultToPlayer(ctx, winnerAddress)
+	k.board.MustAddForfeitedGameResultToPlayer(ctx, loserAddress)
 }
